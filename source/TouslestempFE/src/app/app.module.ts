e7b31af158a46b1986167a6900c590d14c47aae2
imstudent2210 <imstudent2210@gmail.com>
2023-03-14 13:15:53 +0700

    update ui login

--- source/TouslestempFE/src/app/app.module.ts
+++ source/TouslestempFE/src/app/app.module.ts
@@ -4,15 +4,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+//===============================
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatCardModule, MatCardTitleGroup } from '@angular/material/card';
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule, Routes } from "@angular/router";
+import { HttpClientModule } from '@angular/common/http';
+// import { AuthInterceptorProvider } from './services/auth.interceptor';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RegisterComponent } from './components/register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule,
+    MatInputModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    FormsModule
+
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]

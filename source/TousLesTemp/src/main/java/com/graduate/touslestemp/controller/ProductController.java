7ec70370eb8329019b5d7638835c84a7f56346d0
imstudent2210 <imstudent2210@gmail.com>
2023-04-24 11:22:20 +0700

    fix product image to firebase (BE)

--- source/TousLesTemp/src/main/java/com/graduate/touslestemp/controller/ProductController.java
+++ source/TousLesTemp/src/main/java/com/graduate/touslestemp/controller/ProductController.java
@@ -33,7 +33,7 @@ public class ProductController {
         return productService.getAllProduct(request);
     }
 
-    @GetMapping("/get")
+    @GetMapping("/get-all")
     public ResponseEntity<List<ProductDto>> allProductDTO() {
         return new ResponseEntity<>(productMapper.toProductDTOs(productRepository.findAll()), HttpStatus.OK);
     }
@@ -43,15 +43,6 @@ public class ProductController {
         return new ResponseEntity<>(productService.find(id), HttpStatus.OK);
     }
 
-    @GetMapping("/get2")
-    public ResponseEntity<List<Product>> allProduct() {
-        return new ResponseEntity<>(productRepository.findAll(), HttpStatus.OK);
-    }
-
-    @GetMapping("/get2/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable(name = "id") Long id) {
-        return new ResponseEntity<>(productService.findProduct(id), HttpStatus.OK);
-    }
 
 
     @PostMapping("/create")
@@ -79,18 +70,7 @@ public class ProductController {
         return this.productService.filter(categoryId);
     }
 
-
-    @PostMapping(value = {"/create2"}, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_OCTET_STREAM_VALUE,})
-    public ResponseEntity<Product> createProductDTO2(@RequestPart("product") @Valid Product product, @RequestPart("file") MultipartFile[] file) throws Exception {
-        return new ResponseEntity<>(productService.create2(product, file), HttpStatus.OK);
-    }
-
-    @PutMapping(value = "/update2/{id}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_OCTET_STREAM_VALUE,})
-    public ResponseEntity<Product> updateProductDTO2(@RequestPart("product") @Valid Product product, @PathVariable(name = "id") Long id, @RequestPart("file") MultipartFile[] file) throws Exception {
-        return new ResponseEntity<>(productService.update2(product, id, file), HttpStatus.OK);
-    }
-
-    @DeleteMapping("/delete2/{id}")
+    @DeleteMapping("/delete/{id}")
     public void deleteProduct(@PathVariable("id") Long id) throws Exception {
         this.productService.delete(id);
     }
@@ -100,4 +80,10 @@ public class ProductController {
         return this.productRepository.getProductByStoreId(id);
     }
 
+
+
+    //    @GetMapping("/get2")
+//    public ResponseEntity<List<Product>> allProduct() {
+//        return new ResponseEntity<>(productRepository.findAll(), HttpStatus.OK);
+//    }
 }
